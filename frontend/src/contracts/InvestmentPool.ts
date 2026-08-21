9681e388e5707ca43f907afc2331ab8fa1952b36
aleksmiler8 <aleksmiler8@gmail.com>
2026-08-21 21:26:33 +0200

    Update frontend pool address

--- frontend/src/contracts/InvestmentPool.ts
+++ frontend/src/contracts/InvestmentPool.ts
@@ -3,7 +3,7 @@ import { getSigner } from "../provider";
 import { InvestmentPoolABI } from "./InvestmentPoolABI";
 
 const CONTRACT_ADDRESS =
-"0x5B9736101be66185115874FB4Fe76e3746DC4e93";
+  "0xa888b33f2ed28a55511AaA74E6CB66B2E4A49a67";
 
 export async function getContract() {
   const signer = await getSigner();
